572ea103c1032293aaddc7c65b9a8123cfdbc11d
jeff20501 <jeffkimmuna@gmail.com>
2025-08-21 13:21:56 +0300

    .

--- src/BrowserExtensions/Page.jsx
+++ src/BrowserExtensions/Page.jsx
@@ -14,7 +14,7 @@ export function Page(){
         return localStorage.getItem('darkmode')==='active'
     })
     const [tab, setTab]=useState('all')
-
+ 
     //useEffects
     //data fetch
     useEffect(()=>{
